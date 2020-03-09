23a56d29817019c557777703ad65b29d19b97221
huangyongwen <huangyongwen0306@163.com>
2020-03-09 23:42:41 +0800

    1.解决滥用注解报错

--- src/main/java/com/yudianxx/springBootDemo/MeiziTuApplication.java
+++ src/main/java/com/yudianxx/springBootDemo/MeiziTuApplication.java
@@ -1,22 +1,13 @@
 package com.yudianxx.springBootDemo;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //Spring Boot Application的入口点是包含@SpringBootApplication注释的类。
 //		该类应具有运行Spring Boot应用程序的主要方法。 @SpringBootApplication注释包括自动配置，
@@ -27,9 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @RestController
 //@EnableEurekaClient
-@EnableSwagger2
 //使用Hystrix隔离服务
-@EnableHystrix
 @EnableAsync //开启异步加载没空 即扫描 @Async
 //@MapperScan("com.yudianxx.springBootDemo.mapper")
 //@EnableAutoConfiguration(exclude = {
@@ -37,8 +26,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //})
 //@ImportResource(locations = {"classpath:/mybatis/spring-mybatis-plus.xml"})
 @Slf4j
-@EnableAspectJAutoProxy
-public class SpringBootDemoApplication implements CommandLineRunner {
+public class MeiziTuApplication implements CommandLineRunner {
     //public class SpringBootDemoApplication extends   SpringBootServletInitializer {
 //	@Override
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -46,7 +34,7 @@ public class SpringBootDemoApplication implements CommandLineRunner {
 //	}
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        SpringApplication.run(MeiziTuApplication.class, args);
     }
 
     //	spring启动
@@ -54,16 +42,6 @@ public class SpringBootDemoApplication implements CommandLineRunner {
     public void run(String... arg0) throws Exception {
         log.info("springboot服务已启动......");
     }
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .pathMapping("/")
-                .select() // 选择那些路径和api会生成document
-                .apis(RequestHandlerSelectors.any())// 对所有api进行监控
-                .apis(RequestHandlerSelectors.basePackage("com.yudianxx.springBootDemo.controller"))
-                .build();
-    }
-
     //@Bean 的话一定是一个对象，不能是一个void
     @Bean(name = "methods")
     public String method1(){
