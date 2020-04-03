a86992752d3176c257cbc08a9f6fd35f2b316aac
huangyongwen <huangyongwen@meizu.com>
2020-04-03 17:54:07 +0800

    1. 两种方式处理跨域

--- src/main/java/com/yudianxx/springBootDemo/interceptor/WebSecurityConfig.java
+++ src/main/java/com/yudianxx/springBootDemo/interceptor/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.yudianxx.springBootDemo.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -38,4 +39,25 @@ public class WebSecurityConfig extends WebMvcConfigurerAdapter {
 
         super.addInterceptors(registry);
     }
+
+    /**
+     *  解决跨域的问题
+     * @param registry
+     */
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+
+                .allowedOrigins("*")
+
+                .allowCredentials(true)
+
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+
+                .maxAge(3600);
+
+    }
+
 }
